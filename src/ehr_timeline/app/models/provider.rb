50406d9261b89cc3fb9e1855968caef237b07a71
Darshan <darshanvkalola@gmail.com>
2019-11-24 17:44:48 -0500

    Add seed patient and providers.

--- src/ehr_timeline/app/models/provider.rb
+++ src/ehr_timeline/app/models/provider.rb
@@ -2,9 +2,9 @@ class Provider < ApplicationRecord
   has_many :events
   enum provider_type: {
       'physician': 0,
-      'physician_assistant': 1,
+      'physician assistant': 1,
       'nurse': 2,
       'administrator': 3,
-      'lab_technician': 4
+      'lab technician': 4
   }
 end
--- src/ehr_timeline/db/seeds.rb
+++ src/ehr_timeline/db/seeds.rb
@@ -6,4 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # Seed of patients
+patients = Patient.create(
+    [
+        {name: 'Darshan'},
+        {name: 'Bob'},
+        {name: 'Alice'},
+        {name: 'Emma'},
+        {name: 'Olivia'},
+        {name: 'Ava'},
+        {name: 'Sophia'},
+        {name: 'Charlotte'},
+    ]
+)
+providers = Provider.create(
+[
+    {name: 'Alexander', provider_type: 'physician'},
+    {name: 'Liam', provider_type: 'physician'},
+    {name: 'William', provider_type: 'physician'},
+    {name: 'Claire', provider_type: 'physician'},
+
+    {name: 'Benjamin', provider_type: 'physician assistant'},
+    {name: 'Lucas', provider_type: 'nurse'},
+    {name: 'Henry', provider_type: 'administrator'},
+    {name: 'Skylar', provider_type: 'lab technician'},
+  ]
+)
+
+
 
